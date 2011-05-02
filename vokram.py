b27f91003efa89dec0775accc48b5dc3f7af1b74
Will McCutchen <mccutchen@gmail.com>
2011-05-02 12:18:48 -0500

    Add lots of narrative documentation

--- vokram.py
+++ vokram.py
@@ -1,8 +1,78 @@
 #!/usr/bin/env python
 
 """
-A simple, generic implementation of Markov chains in Python, with some
-helpers for generating chains of words.
+A simple, generic implementation of Markov chains in Python, with some helpers
+for generating chains of words. A brief overview of how this works:
+
+ 1. Build a Markov model from a given corpus, which can be a sequence of
+    basically anything (e.g., numbers, words). Special support is provided for
+    building models based on words in strings or file-like objects.
+
+    In this implementation, a model is a dictionary that maps tuples of
+    n-grams to lists of the items that appear after those n-grams in the input
+    corpus. The size of the n-grams is determined by the user, but currently
+    defaults to 2.
+
+    So, taking this simple corpus as an example (where ">>>" represents the
+    interactive Python prompt):
+
+      >>> corpus = [1, 2, 3, 2, 4, 5, 6, 2, 1, 3]
+
+    The model, based on the default n-gram size of 2, would look like this:
+
+      >>> build_model(corpus)
+      {(1, 2): [3, 3, 4],
+       (2, 1): [2],
+       (2, 3): [1, 2],
+       (2, 4): [3],
+       (2, 5): [4],
+       (3, 1): [2],
+       (3, 2): [1, 5],
+       (4, 3): [2, 1],
+       (5, 4): [3]}
+
+    For reference, the model of the same corpus with an n-gram size of 3 would
+    look like this:
+
+      >>> build_model(corpus, n=3)
+      {(1, 2, 3): [1, 2],
+       (1, 2, 4): [3],
+       (2, 1, 2): [4],
+       (2, 3, 1): [2],
+       (2, 3, 2): [1],
+       (2, 4, 3): [2],
+       (2, 5, 4): [3],
+       (3, 1, 2): [3],
+       (3, 2, 1): [2],
+       (3, 2, 5): [4],
+       (4, 3, 2): [5],
+       (5, 4, 3): [1]}
+
+ 2. Once the model is built, we can use it to construct a Markov chain of (I
+    think, though there's a decent chance I'm butchering some or all of these
+    concepts) statistically likely outputs.
+
+    The process for building a chain works like this:
+
+     a. Get a starting key in the model (chosen by the user or chosen
+        randomly). For our purposes, let's choose the key (2, 3) from the
+        first model above.
+
+     b. Pick a random item from the list that our chosen key points to and add
+        it to our chain. Let's say we choose 2 from the list [1, 2].
+
+     c. Build a new key by dropping the first item in our current key and
+        appending the item we chose in step b. This gives us a new key,
+        (3, 2).
+
+     d. Start over at step b, using our new key. In this example, we'd end up
+        choosing a random value from the list [1, 5] to add to our chain. Do
+        this until the chain has reached the desired length.
+
+Note, there is a specialized markov_words version of the markov_chain function
+that tries to slightly better at generating Markov chains that make
+complete-ish sentences by trying to pick good starting keys and ensuring that
+the chain ends in some kind of "sentence-ending" punctuation.
 
 With inspiration from:
 http://code.activestate.com/recipes/194364-the-markov-chain-algorithm/
@@ -57,9 +127,10 @@ def build_model(xs, n=DEFAULT_NGRAM_SIZE):
 ##############################################################################
 
 def markov_words(model, length, start_key=None):
-    """Generates a Markov chain of the given length. Attempts to be
-    intelligent about generating chains made up of what (hopefully) look like
-    complete sentences.
+    """Generates a Markov chain of approximately the given length. Attempts to
+    be intelligent about generating chains made up of what (hopefully) look
+    like complete sentences, which means that the resulting sentence will
+    often have fewer than the desired number of words.
     """
 
     # An overly-simplistic heuristic to use to try to generate complete
